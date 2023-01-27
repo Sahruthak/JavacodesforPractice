5b514291724af5b7884bfd3b3767947ac7156b36
SahruthaReddy <107304838+Sahruthak@users.noreply.github.com>
2023-01-27 18:39:03 +0530

    Create Multi.java

---
+++ Multi.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Multi
+{
+public static void main(String args[])
+{
+int a,b,c;
+Scanner sc=new Scanner(System.in);
+a=sc.nextInt();
+b=sc.nextInt();
+c=a*b;
+System.out.println("Multiplication of two numbers : "+c);
+}
+}
