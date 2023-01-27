8cea438d074542f0c913e50c37bd8f9078db7110
SahruthaReddy <107304838+Sahruthak@users.noreply.github.com>
2023-01-27 18:40:56 +0530

    Create Divi.java

---
+++ Divi.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Divi
+{
+public static void main(String args[])
+{
+float a,b,c;
+Scanner sc=new Scanner(System.in);
+a=sc.nextInt();
+b=sc.nextInt();
+c=a/b;
+System.out.println("Division of two numbers : "+c);
+}
+}
