2541872b27ac699b7754e26f80484358e61b11f2
SahruthaReddy <107304838+Sahruthak@users.noreply.github.com>
2023-01-30 09:56:49 +0530

    Create Parallelogramarea.java

---
+++ Parallelogramarea.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Parallelogramarea
+{
+  public static void main(String args[])
+  {
+  Scanner sc=new Scanner(System.in);
+  double h,b,area;
+  System.out.println("Base");
+  b=sc.nextDouble();
+  System.out.println("Height");
+  h=sc.nextDouble();
+  area=b*h;
+  System.out.println("Area of Parallelogram "+area);
+  }
+}
