7430b1a2cfc2231261a31e7f5b56431834936346
SahruthaReddy <107304838+Sahruthak@users.noreply.github.com>
2023-01-30 09:16:04 +0530

    Create Rectanglearea.java

---
+++ Rectanglearea.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Rectanglearea
+{
+  public static void main(String args[])
+  {
+  Scanner sc=new Scanner(System.in);
+  float l,b,a;
+  System.out.println("length");
+  l=sc.nextFloat();
+  System.out.println("breadth");
+  b=sc.nextFloat();
+  a=l*b;
+  System.out.println("AREA OF RECTANGLE IS "+a);
+  }
+}
