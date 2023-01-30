f30c40d72fd64ed5381de34fe14101db180addab
SahruthaReddy <107304838+Sahruthak@users.noreply.github.com>
2023-01-30 09:31:17 +0530

    Create Circlearea.java

---
+++ Circlearea.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Circlearea
+{
+  public static void main(String args[])
+  {
+  Scanner sc=new Scanner(System.in);
+  double r,a;
+  System.out.println("radius");
+  r=sc.nextDouble();
+  a=(4.0/3.0)*(22.0/7.0)*r*r*r;
+  System.out.println("AREA OF CIRCLE IS "+a);
+  }
+}
