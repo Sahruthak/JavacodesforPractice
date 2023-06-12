be2f64938d055b4842fab16ad6a45e9df8f10958
SahruthaReddy <107304838+Sahruthak@users.noreply.github.com>
2023-06-12 21:53:21 +0530

    Add files via upload

---
+++ Duplicate.java
@@ -0,0 +1,29 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Duplicate {
+    public static boolean containsDuplicate(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : arr) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n,i;
+        n=sc.nextInt();
+        int arr[]=new int[n+1];
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Array 1 contains duplicates: " + containsDuplicate(arr));
+    }  
+}
