d332db1f9d2069b17387d6b3939309ccae3c2785
SahruthaReddy <107304838+Sahruthak@users.noreply.github.com>
2023-06-14 08:01:45 +0530

    Add files via upload

---
+++ Functions in java/Average.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Average
+{
+    public static int avg(int a,int b,int c){
+        int Avg=(a+b+c)/2;
+        return Avg;
+    }
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int a=sc.nextInt();
+	    int b=sc.nextInt();
+	    int c=sc.nextInt();
+	    System.out.println("Average : "+avg(a,b,c));
+       }
+}
