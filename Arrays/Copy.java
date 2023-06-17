a5d9c4f09cec6063ecf678067f9f1b9d708c715a
SahruthaReddy <107304838+Sahruthak@users.noreply.github.com>
2023-06-17 22:47:04 +0530

    Add files via upload

---
+++ Arrays/Copy.java
@@ -0,0 +1,38 @@
+//1) Java Program to copy all elements of one array into another array
+//2) Java Program to find the frequency of each element in the array
+//3) Java Program to left rotate the elements of an array
+//4) Java Program to print the duplicate elements of an array
+//5) Java Program to print the elements of an array
+//6) Java Program to print the elements of an array in reverse order
+//7) Java Program to print the elements of an array present on even position
+//8) Java Program to print the elements of an array present on odd position
+//9) Java Program to print the largest element in an array
+//10) Java Program to print the smallest element in an array
+//11) Java Program to print the number of elements present in an array
+//12) Java Program to print the sum of all the items of the array
+//13) Java Program to right rotate the elements of an array
+//14) Java Program to sort the elements of an array in ascending order
+//15) Java Program to sort the elements of an array in descending order
+//16) Java Program to Find 3rd Largest Number in an array
+//17) Java Program to Find 2nd Largest Number in an array
+//18) Java Program to Find Largest Number in an array
+//19) Java to Program Find 2nd Smallest Number in an array
+//20) Java Program to Find Smallest Number in an array
+//21) Java Program to Remove Duplicate Element in an array
+//22) Java Program to Print Odd and Even Numbers from an array
+//23) How to Sort an Array in Java
+
+import java.util.*;
+class Copy{
+    public static void main(String[] args) {
+    Scanner sc =new Scanner(System.in);
+    int n=sc.nextInt();
+    int a[]=new int[100];
+    for(int i=0;i<n;i++) {
+        a[i]=sc.nextInt();
+    }    
+    for(int i=0;i<n;i++) {
+        System.out.print(a[].count(1)+" ");
+    } 
+    }
+}
