b80639441ad5491ab3434a1e0e1566f897d847da
SahruthaReddy <107304838+Sahruthak@users.noreply.github.com>
2023-06-21 11:24:46 +0530

    Update IsSortedArray.java

--- Recursion programs/IsSortedArray.java
+++ Recursion programs/IsSortedArray.java
@@ -11,8 +11,7 @@ public class IsSortedArray {
             return false;
          }
     }
-    
-    
+   
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
