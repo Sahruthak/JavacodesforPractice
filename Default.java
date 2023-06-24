017da4b4666ae1a25d72ba7a235db79795e063fc
SahruthaReddy <107304838+Sahruthak@users.noreply.github.com>
2023-06-24 09:52:27 +0530

    Add files via upload

---
+++ Default.java
@@ -0,0 +1,11 @@
+class Default
+{
+	public static void main(String[] args) {
+	    int ch;
+            char c;
+	    String str;
+	    System.out.println(str);
+	    System.out.println(ch);
+            System.out.println(c);
+	}
+}
