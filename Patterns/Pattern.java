179c3689fa296d3a4b3b498712a2fdba1742a36e
SahruthaReddy <107304838+Sahruthak@users.noreply.github.com>
2023-06-25 23:18:37 +0530

    Add files via upload

---
+++ Patterns/Pattern.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Pattern
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    for(int i=0;i<n;i++)
+	    {
+	        for(int j=n;j>0;j--)
+	        {
+	            if(j==i+1)
+	            System.out.print("*");
+	            else
+	            System.out.print(j+" ");
+	        }
+	    System.out.println();
+	    }
+	}
+}
+
+
