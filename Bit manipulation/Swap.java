bc2653f90b8874762352da6624e1fa860fd6aa29
SahruthaReddy <107304838+Sahruthak@users.noreply.github.com>
2023-06-26 22:53:56 +0530

    Add files via upload

---
+++ Bit manipulation/Swap.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Swap
+{
+	public static void main(String[] args) {
+	    Scanner c=new Scanner(System.in);
+	    int a=c.nextInt();
+	    int b=c.nextInt();
+	    a=a^b;
+	    b=b^a;
+	    a=a^b;
+		System.out.println("a : "+a);
+		System.out.println("b : "+b);
+	}
+}
