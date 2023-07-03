01f3fc5daa6eaa9c51f642bf6d1074042ea9da39
SahruthaReddy <107304838+Sahruthak@users.noreply.github.com>
2023-07-03 08:33:16 +0530

    Add files via upload

---
+++ maths/Josephus.java
@@ -0,0 +1,15 @@
+/**
+Josephus problem
+**/
+
+public class Josephus
+{
+	public static void main(String[] args) {
+	    int ans=josephus(5,3);
+		System.out.println("Alive "+ans);
+	}
+	static int josephus(int n,int k){
+	if(n==1) return 0;
+	return (josephus(n-1,k)+k)%n;
+	}
+}
