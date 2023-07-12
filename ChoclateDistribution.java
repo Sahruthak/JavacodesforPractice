2336eceddb1836c3e5da4ad8ad05c1555834a2e7
SahruthaReddy <107304838+Sahruthak@users.noreply.github.com>
2023-07-12 23:38:41 +0530

    Create ChoclateDistribution.java

---
+++ ChoclateDistribution.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+public class ChoclateDistribution{
+  static int findMinDiff(int arr[], int n, int m) {
+        if (m == 0 || n == 0)
+            return 0;
+ 
+        Arrays.sort(arr);
+ 
+        if (n < m)
+            return -1;
+ 
+        int min_diff = Integer.MAX_VALUE;
+ 
+        for (int i = 0; i + m - 1 < n; i++) {
+            int diff = arr[i + m - 1] - arr[i];
+            if (diff < min_diff)
+                min_diff = diff;
+        }
+        return min_diff;
+    }
+  
+    public static void main(String[] args) {
+        int[] arr = {7, 3, 2, 4, 9, 12, 56};
+        int m = 3;
+        int n = arr.length;
+
+        int minDiff = findMinDiff(arr, n, m);
+        System.out.println("Minimum Difference is: " + minDiff);
+    }
+}
