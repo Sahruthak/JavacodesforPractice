cc67d7adfb04be43adc2b8700168f7be874e7e1b
SahruthaReddy <107304838+Sahruthak@users.noreply.github.com>
2023-07-26 23:28:11 +0530

    Update Copy.java

--- Arrays/Copy.java
+++ Arrays/Copy.java
@@ -4,7 +4,6 @@
 //4) Java Program to print the duplicate elements of an array
 //5) Java Program to print the elements of an array
 //6) Java Program to print the elements of an array in reverse order
-
 //7) Java Program to print the elements of an array present on even position
 
 //8) Java Program to print the elements of an array present on odd position
