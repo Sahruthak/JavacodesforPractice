9b7874f7d69715a7c2f5b55e0151bfb1b76d28c8
SahruthaReddy <107304838+Sahruthak@users.noreply.github.com>
2023-09-05 21:39:57 +0530

    Update ReverseSinglyLinkedList.java

--- ReverseSinglyLinkedList.java
+++ ReverseSinglyLinkedList.java
@@ -3,10 +3,8 @@ class ReverseSinglyLinkedList {
         if (head == null || head.next == null) {
             return head;
         }
-
         ListNode prev = null;
         ListNode current = head;
-
         while (current != null) {
             ListNode next = current.next;
             current.next = prev;
