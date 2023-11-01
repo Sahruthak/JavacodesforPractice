f2b1068475cefe4a3a4243621b033772e400d734
Sahrutha Reddy <107304838+Sahruthak@users.noreply.github.com>
2023-11-01 07:37:23 +0530

    Add files via upload

--- Prime.java
+++ Prime.java
@@ -1,18 +1,20 @@
-import java.util.*;
-public class Prime
-{
-	public static void main(String[] args) {
-	    int d=0,i,n;
-	    Scanner sc=new Scanner(System.in);
-	    n=sc.nextInt();
-	    for(i=1;i<n;i++)  
-	    {
-	        if(n%i==0)
-	        d++;
-	    }
-	    if(d>2)
-	    System.out.print("Not prime");
-	    else
-	    System.out.print("Prime");
-	}
+class Prime {
+    public static void main(String[] args) {
+        int m = 2;
+        int c = 0;
+        for (int i = 2; i <= Math.sqrt(m); i++) { // Change the loop condition
+            if (m % i == 0) {
+                c++;
+            }
+
+            if (c > 1) {
+                System.out.println("not prime");
+                break;
+            }
+        }
+
+        if (c <= 1) { // Check if c is less than or equal to 1
+            System.out.println("Prime");
+        }
+    }
 }
