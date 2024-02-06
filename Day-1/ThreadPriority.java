17fab087e099bf15e01560631769f28e4968198a
Sahrutha Reddy <107304838+Sahruthak@users.noreply.github.com>
2024-02-06 09:59:36 +0530

    Create ThreadPriority.java

---
+++ Day-1/ThreadPriority.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main extends Thread
+{
+	public static void main(String[] args) {
+        Main m1=new Main();
+        Main m2=new Main();
+        m1.setPriority(Thread.MIN_PRIORITY);
+        m2.setPriority(Thread.MAX_PRIORITY);
+        m1.start();
+        m2.start();
+	}
+	public void run(){
+	    System.out.println("Thread " +Thread.currentThread().getName()+"   "+"Running thread " +Thread.currentThread().getPriority());
+	}
+}
+
+
+
