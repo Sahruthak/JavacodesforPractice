01e191e30028732ee15fa27c5a18da091e7805b3
Sahrutha Reddy <107304838+Sahruthak@users.noreply.github.com>
2024-03-16 22:32:50 +0530

    Update Circlearea.java

--- Circlearea.java
+++ Circlearea.java
@@ -4,7 +4,8 @@ class Circlearea
   public static void main(String args[])
   {
   Scanner sc=new Scanner(System.in);
-  double r,a;
+  double r;
+  double a;
   System.out.println("radius");
   r=sc.nextDouble();
   a=(4.0/3.0)*(22.0/7.0)*r*r*r;
