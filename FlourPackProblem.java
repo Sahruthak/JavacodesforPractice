c81daf74a5bacb59c733fe377daf48513c2c8b2f
Sahrutha Reddy <107304838+Sahruthak@users.noreply.github.com>
2024-08-07 11:19:21 +0530

    Add files via upload

---
+++ FlourPackProblem.java
@@ -0,0 +1,9 @@
+public class FlourPackProblem {
+    public static void main(String[] args) {
+       int ans=canPack(12,2,3);
+       System.out.println(ans);
+    }
+    public static int canPack(int bigCount, int smallCount, int goal){
+
+    }
+}
